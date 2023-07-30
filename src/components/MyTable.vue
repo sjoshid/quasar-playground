0f7dd008e2d36551aa0acd3ab7c7060bd14054fa
Sujit Joshi <joshi.sujit020386@gmail.com>
2023-07-30 16:23:00 -0400

    Initial commit

---
+++ src/components/MyTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="q-pa-md">
+    <q-table
+      :columns="tableCols"
+      :rows="tableData"
+      selection="single"
+      title="Sample"
+      row-key="name"
+    >
+      <template #loading> Loading content...</template>
+    </q-table>
+    <p>Value of update col is {{ showUpdateCol }}</p>
+    <p>Value of delete col is {{ showDeleteCol }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { tableCols, tableData } from '../stores/dummy-store';
+
+const props = defineProps({
+  showUpdateCol: Boolean,
+  showDeleteCol: Boolean,
+});
+</script>
---
+++ src/router/routes.ts
@@ -0,0 +1,24 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('components/MyTable.vue'),
+        props: { showDeleteCol: false, showUpdateCol: false },
+      },
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;
